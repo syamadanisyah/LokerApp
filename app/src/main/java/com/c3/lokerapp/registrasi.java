bf55513f77caf1b8ca2d526d62b6681c8e3b9fff
syamadanisyah <syamadanisyah.syamadanisyah@gamil.com>
2023-09-22 15:04:37 +0700

    1.1

---
+++ app/src/main/java/com/c3/lokerapp/registrasi.java
@@ -0,0 +1,14 @@
+package com.c3.lokerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registrasi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrasi);
+    }
+}
