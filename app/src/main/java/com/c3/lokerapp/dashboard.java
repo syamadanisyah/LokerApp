c80202865f5f3202a5d329bb671c75d31a26ab54
syamadanisyah <syamadanisyah.syamadanisyah@gamil.com>
2023-10-01 10:45:29 +0700

    home dashboard navbar

--- app/src/main/java/com/c3/lokerapp/dashboard.java
+++ app/src/main/java/com/c3/lokerapp/dashboard.java
@@ -1,14 +1,45 @@
 package com.c3.lokerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
-public class dashboard extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+public class dashboard extends AppCompatActivity {
+BottomNavigationView nav;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+
+        nav = findViewById(R.id.navigasi_bottom);
+nav.setOnNavigationItemSelectedListener(item -> {
+    Fragment selectFragment = null;
+
+    if (item.getItemId()==R.id.home){
+        selectFragment = new home();
+    }
+    else if(item.getItemId()==R.id.simpan){
+        selectFragment = new simpan();
+    }
+
+    else if(item.getItemId()==R.id.akun){
+        selectFragment = new akun();
+    }
+
+    if (selectFragment != null){
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.frame,selectFragment)
+                .commit();
+    }
+    return true;
+});
+
+getSupportFragmentManager().beginTransaction()
+        .replace(R.id.frame,new home()).commit();
+
     }
 }
